f773264984835e38e263112c04c232f15edec5fa
Ari Weinberg <ari@ari.com>
2026-06-17 15:16:06 +0300

    \doc: document DBConnection class.

--- database/db_connection.py
+++ database/db_connection.py
@@ -3,6 +3,7 @@ from mysql.connector.abstracts import MySQLConnectionAbstract
 
 
 class DBConnection:
+    """DBConnection is a simple mysql connection and server manager."""
     def __init__(self,
                  host: str | None = None,
                  port: str | None = None,
@@ -10,17 +11,46 @@ class DBConnection:
                  password: str | None = None,
                  database: str | None = None
                  ):
+        """Initializes the manager, by setting its parameters."""
         pass
 
     def get_connection(self) -> MySQLConnectionAbstract:
+        """
+        Returns a connection to the server.
+
+        Note that this method returns a connection to a spesific database.
+
+        Caller MUST ensure the database exists.
+        """
         pass
 
     def create_database(self) -> None:
+        """
+        Creates the database if it doesn't already exist.
+
+        Note that this method uses its own connection to avoid trying to
+            connect to a non-existant database.
+        """
         pass
 
     def create_tables(self) -> None:
+        """
+        Creates the tables needed for this app.
+
+        The tables created by this method are: `agents` and `missions`. 
+
+        Caller MUST ensure the database exists.
+        """
         pass
 
     @contextmanager
     def __call__(self, *args, **kwds):
+        """
+        Adds callability to every instance.
+        
+        Makes the instance act as a context manager yeilding an already
+            initialized cursor and managing the connection afterwards.
+
+        Ensures database creation.
+        """
         pass
