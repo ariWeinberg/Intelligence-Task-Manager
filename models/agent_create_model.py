e88c5837d93e01e0878e202fb75f53b04537968c
Ari Weinberg <ari@ari.com>
2026-06-18 10:27:01 +0300

    \re: fixed agent models to adopt new rules (all fields on update and create.).

--- models/agent_create_model.py
+++ models/agent_create_model.py
@@ -3,6 +3,10 @@ from pydantic import BaseModel, Field
 
 
 class AgentCreateModel(BaseModel):
-        agent_name: str = Field(alias="name", max_length=50)
-        agent_specialty: str = Field(alias="specialty", max_length=50)
-        agent_agent_rank: Literal['Junior', 'Senior', 'Commander'] = Field(alias="agent_rank")
+    agent_name: str = Field(alias="name", max_length=50)
+    agent_specialty: str = Field(alias="specialty", max_length=50)
+    agent_is_active: bool = Field(alias="is_active", default=True)
+    agent_completed_missions: int = Field(alias="completed_missions", default=0)
+    agent_failed_missions: int = Field(alias="failed_missions", default=0)
+    agent_agent_rank: Literal['Junior', 'Senior', 'Commander'] = Field(alias="agent_rank")
+
