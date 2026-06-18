be2ce483a0369d1dd2f6008110154660444fc9d8
Ari Weinberg <ari@ari.com>
2026-06-18 11:29:49 +0300

    \fix: fix alias of mission status field.

--- models/mission_create_model.py
+++ models/mission_create_model.py
@@ -1,6 +1,6 @@
 from typing import Literal
 from pydantic import BaseModel, Field
-from models.types import MissionStatus
+from models.types import MissionRiskLevel, MissionStatus
 
 
 class MissionCreateModel(BaseModel):
@@ -9,5 +9,19 @@ class MissionCreateModel(BaseModel):
     mission_location: str = Field(alias="location", max_length=50)
     mission_difficulty: int = Field(alias="difficulty", ge=1, le=10)
     mission_importance: int = Field(alias="importance", ge=1, le=10)
-    mission_status: MissionStatus = Field(alias="'status", default='NEW')
+    mission_status: MissionStatus = Field(alias="status", default='NEW')
     mission_assigned_agent_id: int | None = Field(alias="assigned_agent_id", default=None)
+
+    def risk_level(self) -> MissionRiskLevel:
+        level = self.mission_difficulty * 2 + self.mission_importance
+
+        if level < 10:
+            return 'LOW'
+
+        if level < 18:
+            return 'MEDIUM'
+
+        if level < 25:
+            return 'HIGH'
+
+        return 'CRITICAL'
--- models/mission_view_model.py
+++ models/mission_view_model.py
@@ -10,6 +10,6 @@ class MissionViewModel(BaseModel):
     mission_location: str = Field(alias="location", max_length=50)
     mission_difficulty: int = Field(alias="difficulty", ge=1, le=10)
     mission_importance: int = Field(alias="importance", ge=1, le=10)
-    mission_status: MissionStatus = Field(alias="'status", default='NEW')
+    mission_status: MissionStatus = Field(alias="status", default='NEW')
     mission_risk_level: MissionRiskLevel = Field(alias="'risk_level")
     mission_assigned_agent_id: int | None = Field(alias="assigned_agent_id", default=None)
